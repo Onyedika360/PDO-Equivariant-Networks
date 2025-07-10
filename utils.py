8b1e09166d291218134fe24d29f109242ee5909a
Onyedikachukwu Ojukwu <113629943+Onyedika360@users.noreply.github.com>
2025-07-10 11:00:01 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,11 @@
+class StandardScaler3D(BaseEstimator,TransformerMixin):
+    #batch, sequence, channels
+    def __init__(self):
+        self.scaler = StandardScaler()
+
+    def fit(self,X,y=None):
+        self.scaler.fit(X.reshape(-1, X.shape[2]))
+        return self
+
+    def transform(self,X):
+        return self.scaler.transform(X.reshape( -1,X.shape[2])).reshape(X.shape)
